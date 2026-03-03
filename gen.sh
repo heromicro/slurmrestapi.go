0322842bea223bebeaac0df24bd4e2103ddcdb2d
nono <no@no.com>
2026-03-03 15:19:45 +0800

    slurm 25.11.3

--- gen.sh
+++ gen.sh
@@ -1,2 +1,2 @@
 
-openapi-generator generate -i spec/openapi.v0.0.39.json  -g go --package-name slurmrestapi -o ./
+openapi-generator generate -i spec/openapi.25.11.3.json -g go --package-name slurmrestapi -o ./slurmapi
